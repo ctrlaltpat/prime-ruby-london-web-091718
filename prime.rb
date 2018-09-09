2f9325eeeac6d68a22a4443a8948a9969c6b97a5
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 10:06:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(int)
   return false if int < 2
   numbers_to_check = (2..int).to_a
   numbers_to_check.each do |num|
-    if Math.sqrt(int) % num == 0 && num 
+    if Math.sqrt(int) % num == 0
       return false
     end
   end
